f48c2acef9418a1052306b63a73f45d929d906ba
Newland-A <ajn252@gmail.com>
2020-11-02 01:33:53 -0500

    created initial migrations and routes

--- db/migrate/20201102050744_create_users.rb
+++ db/migrate/20201102050744_create_users.rb
@@ -1,8 +1,16 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :username
+      t.string :password_digest
+      t.boolean :is_vet
+      t.boolean :is_owner
+      t.integer :owner_id
+      t.index ["username"], name: "index_users_on_username", unique: true
 
-      t.timestamps
+      t.timestamps null: false
     end
   end
 end
--- db/migrate/20201102050759_create_animals.rb
+++ db/migrate/20201102050759_create_animals.rb
@@ -1,7 +1,15 @@
 class CreateAnimals < ActiveRecord::Migration[6.0]
   def change
     create_table :animals do |t|
-
+      t.string :name
+      t.integer :age
+      t.boolean :gender #how can I create multiple options for gender
+      t.integer :weight
+      t.string :species
+      t.string :breed
+      t.string :color
+      t.text :medical_info
+       
       t.timestamps
     end
   end
